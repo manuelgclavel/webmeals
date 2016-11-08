a1d587c2298245b885cf2d0e4cffc1769557e75c
clavel <manuelgclavel@gmail.com>
2016-11-08 18:17:22 +0700

    initial commit

---
+++ RestAccount/src/com/whitepebbles/restaccount/KeyResource.java
@@ -0,0 +1,15 @@
+package com.whitepebbles.restaccount;
+
+public class KeyResource {
+	private String key;
+
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+}
